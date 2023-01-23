6a2ebccd7e7e5796e3b1cbabdd63ebeef074835c
sahil khatri <sahilkhatri1170@gmail.com>
2023-01-23 20:47:09 +0530

    update db

--- server.js
+++ server.js
@@ -10,9 +10,13 @@ process.on('uncaughtException', err => {
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
 
 mongoose
-  .connect(process.env.DATABASE_LOCAL, {
+  .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,

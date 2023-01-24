a12b5a95eb2dabd318aee62ab741eb25dc5d8fc1
sahil khatri <sahilkhatri1170@gmail.com>
2023-01-24 13:21:59 +0530

    improved stripe

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean'); 
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
 const compression = require('compression');
 const cors = require('cors');
 
@@ -18,7 +17,7 @@ const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const viewRouter = require('./routes/viewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
-
+const bookingController = require('./controllers/bookingController')
 // Start express app
 const app = express();
 
@@ -61,11 +60,11 @@ const limiter = rateLimit({
 app.use('/api', limiter);
 
 // Stripe webhook, BEFORE body-parser, because stripe needs the body as stream
-// app.post(
-//   '/webhook-checkout',
-//   express.raw({ type: 'application/json' }),
-//   bookingController.webhookCheckout
-// );
+app.post(
+  '/webhook-checkout',
+  express.raw({ type: 'application/json' }),
+  bookingController.webhookCheckout
+);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,5 +1,7 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Tour = require('../models/tourModel');
+const User = require('../models/userModel');
+
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 const Booking = require('../models/bookingModel')
@@ -12,7 +14,7 @@ exports.getCheckoutSession =catchAsync( async (req,res,next)=>{
     const session = await stripe.checkout.sessions.create({
         payment_method_types:['card'],
         mode: 'payment',
-        success_url: `${req.protocol}://${req.get('host')}/?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price * 20}`,
+        success_url: `${req.protocol}://${req.get('host')}/my-tours`,
         cancel_url: `${req.protocol}://${req.get('host')}/tours/${tour.slug}`,
         customer_email: req.user.email,
         client_reference_id: req.params.tourId,
@@ -40,15 +42,36 @@ exports.getCheckoutSession =catchAsync( async (req,res,next)=>{
     })
 })
 
-exports.createBookingCheckout =catchAsync(async (req,res,next)=>{
-    //This is only Temporary , because this is unsecure everyone can make bookings withour paying 
+// exports.createBookingCheckout =catchAsync(async (req,res,next)=>{
+//     //This is only Temporary , because this is unsecure everyone can make bookings withour paying 
 
-    const {tour,user,price}= req.query;
-    if(!tour || !user || !price)return next();
-    await Booking.create({tour,user,price});
+//     const {tour,user,price}= req.query;
+//     if(!tour || !user || !price)return next();
+//     await Booking.create({tour,user,price});
     
-    res.redirect(req.originalUrl.split('?')[0]);
-})
+//     res.redirect(req.originalUrl.split('?')[0]);
+// })
+
+const createBookingCheckout =async session => {
+    const tour = client_reference_id;
+    const user = (await User.findOne({email:session.customer_email})).id;
+    const price = session.line_items.price_data.unit_amount /100;
+    await Booking.create({tour,user,price});
+}
+
+exports.webhookCheckout = (req,res,next) =>{
+ const signature = req.headers['stripe-signature'];
+ let event;
+   try{ const event = stripe.webhooks.constructEvent(req.body, signature, process.env.STRIPE_WEBHOOK_SECRET);
+   }catch(err){
+    return res.status(400).send(`Webhook error : ${err.message}`);
+   }
+
+   if(event.type == 'checkout.session.completed')
+    createBookingCheckout(event.data.object)
+
+    res.status(200).json({received:true});
+}
 
 exports.createBooking = factory.createOne(Booking);
 exports.getBooking = factory.getOne(Booking);

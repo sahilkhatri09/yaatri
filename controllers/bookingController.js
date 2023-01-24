f20d9decff28321d9064e0ae77c115b2ba3b7e2e
sahil khatri <sahilkhatri1170@gmail.com>
2023-01-24 13:27:12 +0530

    improved stripe

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -27,7 +27,7 @@ exports.getCheckoutSession =catchAsync( async (req,res,next)=>{
                     product_data: {
                         name: `${tour.name} Tour`,
                         description: tour.summary,
-                        images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+                        images: [`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`],
                     },
                 },
             },
@@ -55,7 +55,7 @@ exports.getCheckoutSession =catchAsync( async (req,res,next)=>{
 const createBookingCheckout =async session => {
     const tour = client_reference_id;
     const user = (await User.findOne({email:session.customer_email})).id;
-    const price = session.line_items.price_data.unit_amount /100;
+    const price = session.line_items[0].price_data.unit_amount /100;
     await Booking.create({tour,user,price});
 }
 
